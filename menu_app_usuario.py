00c17c4b3744330098f4187088eac636c1375677
Josue Arce <jska2.1@gmail.com>
2022-05-08 15:39:30 -0400

    Add seleccionar in menu, change view debug, only view info

---
+++ menu_app_usuario.py
@@ -0,0 +1,24 @@
+from usuario_dao import UsuarioDao
+from logger_base import log
+
+opcion = None
+while opcion != 5:
+    print('Opciones: ')
+    print('1. Listar usuarios')
+    print('2. Agregar usuario')
+    print('3. Modificar usuario')
+    print('4. Eliminar usuario')
+    print('5. Salir')
+    opcion = int(input('Escribe tu opcion (1-5): '))
+    if opcion == 1:
+        usuarios = UsuarioDao.seleccionar()
+        for usuario in usuarios:
+            log.info(usuario)
+
+
+else:
+    log.info("Salimos de nuestra aplicacion .......")
+
+
+
+
